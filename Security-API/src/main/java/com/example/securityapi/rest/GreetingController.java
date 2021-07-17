5a806b50eac67cfd8357ed080ae92c32aca86e3a
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-07-17 11:55:38 +0700

    Update

--- Security-API/src/main/java/com/example/securityapi/rest/GreetingController.java
+++ Security-API/src/main/java/com/example/securityapi/rest/GreetingController.java
@@ -1,12 +1,14 @@
 package com.example.securityapi.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/api")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";

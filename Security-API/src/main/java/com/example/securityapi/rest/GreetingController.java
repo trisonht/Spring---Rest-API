f2449197856d22a699397f9ca9396c7a01083ce0
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-07-17 11:32:49 +0700

    Security - API

---
+++ Security-API/src/main/java/com/example/securityapi/rest/GreetingController.java
@@ -0,0 +1,44 @@
+package com.example.securityapi.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting Greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

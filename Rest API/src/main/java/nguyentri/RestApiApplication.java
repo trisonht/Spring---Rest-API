8c8b5d073cdf67ef2517fcaf1255ad57efcda8a0
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-07-17 07:37:48 +0700

    Greeting - API

---
+++ Rest API/src/main/java/nguyentri/RestApiApplication.java
@@ -0,0 +1,13 @@
+package nguyentri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiApplication.class, args);
+    }
+
+}
---
+++ Rest API/src/main/java/nguyentri/rest/Greeting.java
@@ -0,0 +1,42 @@
+package nguyentri.rest;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+    public long getId() { return id; }
+
+    public String getContent() { return content; }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
